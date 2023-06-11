7f3a12c8592ac2f62ca1e01ba38210eefe919945
julio cesar <jcmcf2012@gmail.com>
2023-06-11 10:47:26 -0300

    infra: add necessary files

--- rockbandWebsite/settings.py
+++ rockbandWebsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "bands.apps.BandsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
