f90f30ffb3cc659577f28633aae52b6488a4ffcb
julio cesar <jcmcf2012@gmail.com>
2023-06-11 10:45:49 -0300

    feat: add urls for bands

--- bands/urls.py
+++ bands/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("add", views.add_band, name="add_band"),
+    path("get", views.get_bands, name="get_bands"),
+    path("delete", views.delete_bands, name="delete_bands")
 ]
